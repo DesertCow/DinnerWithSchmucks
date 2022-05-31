3cc557c44c0d52d5e323d1a4dfcb72e9fe38538e
Clayton S <desertcow07@gmail.com>
2022-05-31 15:30:08 -0700

    End of session

--- assets/js/dinnerScript.js
+++ assets/js/dinnerScript.js
@@ -6,15 +6,17 @@
 
 // Grab Display Fields in HTML
 
-var recpieNameTitleEL = document.querySelector(".recpieNameTitle");
+var recipeNameTitleEL = document.querySelector(".recipeNameTitle");
 
-var recpieCardNameTitleEL = document.querySelector(".recpieCardNameTitle");
-var recpieCardFullRecpieEL = document.querySelector(".recpieCardFullRecpie");
+var recipeCardNameTitleEL = document.querySelector(".recipeCardNameTitle");
+var recipeCardFullrecipeEL = document.querySelector(".recipeCardFullrecipe");
 
 var beerCardBeerTitleEL = document.querySelector(".beerCardBeerTitle");
 var beerCardBeerFactsEL = document.querySelector(".beerCardBeerFacts");
 
 var summaryPageButton = document.querySelector(".summaryButton");
+
+var beerImgEL = document.querySelector(".beerImg");
 
 
 
@@ -22,11 +24,15 @@ var summaryPageButton = document.querySelector(".summaryButton");
 
 summaryPageButton.addEventListener("click", function (clickEvent) {
 
-  recpieCardNameTitleEL.textContent = "Daves Famous Bruger";
-  recpieNameTitleEL.textContent = recpieCardNameTitleEL.textContent;
-  recpieCardFullRecpieEL.textContent = "This is where all the detailed recpie information goes....";
-  beerCardBeerTitleEL.textContent = "Bud Light";
+  recipeCardNameTitleEL.textContent = "Daves Famous Bruger";
+  recipeNameTitleEL.textContent = recipeCardNameTitleEL.textContent;
+  recipeCardFullrecipeEL.textContent = "This is where all the detailed recipe information goes....";
+  beerCardBeerTitleEL.textContent = "Fosters";
   beerCardBeerFactsEL.textContent = "Taste like beer";
+
+  beerImgEL.src = "./assets/img/FOSTERS-Beer.png";
+  beerCardBeerFactsEL.textContent = "The first beer in the world was brewed by the ancient Chinese around the year 7000 BCE (known as kui). In the west, however, the process now recognized as beer brewing began in Mesopotamia at the Godin Tepe settlement now in modern - day Iran between 3500 - 3100 BCE."
+
 
 });
 
@@ -34,14 +40,15 @@ summaryPageButton.addEventListener("click", function (clickEvent) {
 
 summaryPageButton.addEventListener('mouseover', function handleMouseOver() {
 
-  summaryPageButton.style.backgroundColor = '#F46269';
+  summaryPageButton.style.backgroundColor = '#63863C';
+  document.getElementById("summaryButtonShadow").style.boxShadow = "10px 10px 50px 50px #C2CAB9";
 
 });
 
 summaryPageButton.addEventListener('mouseout', function handleMouseOut() {
 
-  summaryPageButton.style.backgroundColor = '#13a92c';
-
+  summaryPageButton.style.backgroundColor = '#C3D9AA';
+  document.getElementById("summaryButtonShadow").style.boxShadow = "none";
 });
 
 // ========================== MAIN ==========================
