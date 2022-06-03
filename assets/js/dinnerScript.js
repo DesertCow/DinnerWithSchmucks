4c197e1aa334c8181d53b6189ad5f16b795e544e
Clayton S <desertcow07@gmail.com>
2022-06-02 21:53:14 -0700

    Some UI bugs/polish needed but displaying all information properly | Random Meal API now working

--- assets/js/dinnerScript.js
+++ assets/js/dinnerScript.js
@@ -7,6 +7,7 @@
 // Grab Display Fields in HTML
 
 var recipeNameTitleEL = document.querySelector(".recipeNameTitle");
+var titleDrinkEL = document.querySelector(".titleDrink");
 
 var recipeCardNameTitleEL = document.querySelector(".recipeCardNameTitle");
 var recipeCardFullrecipeEL = document.querySelector(".recipeCardFullrecipe");
@@ -27,7 +28,11 @@ let summary = {
   "movieTitle": "SNAKES ON A PLANE",
   "drinkName": "Irish Carbomb",
   "drinkInstructions": "Put the lime in the coconut...",
-  "drinkPictureURL": "https://www.thecocktaildb.com/images/media/drink/kugu2m1504735473.jpg"
+  "drinkPictureURL": "https://www.thecocktaildb.com/images/media/drink/kugu2m1504735473.jpg",
+  "mealName": "Brie wrapped in prosciutto & brioche",
+  "mealInstructions": "Mix the flour, 1 tsp salt, ...",
+  "mealPictureURL": "VOID",
+  "mealYouTubeURL": "VOID"
 }
 
 
@@ -82,15 +87,15 @@ async function getRandomDrink() {
     })
     .then(function (data) {
 
-      console.log(data);
+      // console.log(data);
 
       summary.drinkName = data.drinks[0].strDrink;
       summary.drinkInstructions = data.drinks[0].strInstructions;
       summary.drinkPictureURL = data.drinks[0].strDrinkThumb;
 
-      console.log("Random Drink = " + summary.drinkName);
-      console.log("Drink Instructions = " + summary.drinkInstructions);
-      console.log("Drink Picture = " + summary.drinkPictureURL);
+      //console.log("Random Drink = " + summary.drinkName);
+      //console.log("Drink Instructions = " + summary.drinkInstructions);
+      //console.log("Drink Picture = " + summary.drinkPictureURL);
 
       beerCardBeerTitleEL.textContent = summary.drinkName;
       beerImgEL.src = summary.drinkPictureURL;
@@ -101,14 +106,57 @@ async function getRandomDrink() {
     })
 }
 
+async function getRandomDinner() {
+
+  var apiURL = "https://www.themealdb.com/api/json/v1/1/random.php";
+
+  fetch(apiURL)
+    .then(function (respone) {
+
+      if (respone.status != 200) {
+        console.log("ERROR API(" + respone.status + ") from " + apiURL);
+      } else {
+        return respone.json();
+      }
+      return;
+
+    })
+    .then(function (data) {
+
+      summary.mealName = data.meals[0].strMeal;
+      summary.mealInstructions = data.meals[0].strInstructions;
+      summary.mealPictureURL = data.meals[0].strMealThumb;
+      summary.mealYouTubeURL = data.meals[0].strYoutube;
+
+      console.log(summary.mealName);
+      console.log(summary.mealInstructions);
+      console.log(summary.mealPictureURL);
+      console.log(summary.mealYouTubeURL);
+
+      recipeCardNameTitleEL.textContent = summary.mealName;
+      // recipeImgEL.src = summary.mealPictureURL;
+      recipeImgEL.src = 'https://www.themealdb.com/images/media/meals/1520084413.jpg';
+      recipeCardFullrecipeEL.textContent = summary.mealInstructions;
+
+
+      titleDrinkEL.textContent = summary.drinkName;
+      recipeNameTitleEL.textContent = summary.mealName;
+
+      return;
+    })
+}
+
 function init() {
 
   //Load data from session storeage...
 }
 
-// ========================== MAIN ==========================
+// ========================== MAIN START ==========================
 
 init();
 
 getRandomDrink();
+getRandomDinner();
 
+
+// ========================== MAIN END ==========================
